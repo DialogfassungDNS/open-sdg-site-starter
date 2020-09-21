e5387a8c3182ffa6e9f26edd35c74c9268f447e4
MoBosse <moritz.bosse@destatis.de>
2020-09-21 09:55:58 +0000

    Automated deployment to GitHub Pages: d6e0d68b3ca7b6f2079eadcc8ae08810c6b3cf08

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1256,8 +1256,11 @@ var indicatorDataStore = function(dataUrl) {
 
       getCombinationDescription = function(combination) {
         return _.map(Object.keys(combination), function(key) {
-          return translations.t(combination[key]);
-          //return translations.t(key) + ': ' + translations.t(combination[key]);
+          if(key == 'total' or key == 'a) total'){
+            return translations.t(key) + ': ' + translations.t(combination[key]);
+          } else {
+            return translations.t(combination[key]);
+          }
         }).join(', ');
       },
 
