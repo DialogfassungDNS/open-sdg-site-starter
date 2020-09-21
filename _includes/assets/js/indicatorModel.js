6917a497dd6870d321cd5fc36bc6790144989725
Moritz Bosse <50657629+MoBosse@users.noreply.github.com>
2020-09-21 12:08:27 +0200

    Update indicatorModel.js

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -410,7 +410,7 @@ var indicatorModel = function (options) {
 
       getCombinationDescription = function(combination) {
         return _.map(Object.keys(combination), function(key) {
-          if(key == 'total' || key == 'a) total'){
+          if(combination[key] == 'total' || combination[key] == 'a) total'){
             return translations.t(key) + ': ' + translations.t(combination[key]);
           } else {
             return translations.t(combination[key]);
