ab75fe961de1d74b4a891925e338dccd21283e3c
Moritz Bosse <50657629+MoBosse@users.noreply.github.com>
2020-10-26 10:48:02 +0100

    Update indicatorView.js

--- _includes/assets/js/indicatorView.js
+++ _includes/assets/js/indicatorView.js
@@ -713,7 +713,9 @@ var indicatorView = function (model, options) {
 
   this.createSelectionsTable = function(chartInfo) {
     //---#19 addUnitToTableHeaderIfNeeded---start---------------------------------------------------
-    var tableUnit = (chartInfo.selectedUnit && !chartInfo.footerFields[translations.indicator.unit_of_measurement]) ? translations.t(chartInfo.selectedUnit) : '';
+    //---Edit from 26.10.20: Add all Units to headline
+    //var tableUnit = (chartInfo.selectedUnit && !chartInfo.footerFields[translations.indicator.unit_of_measurement]) ? translations.t(chartInfo.selectedUnit) : '';
+    var tableUnit = (chartInfo.selectedUnit ? translations.t(chartInfo.selectedUnit) : chartInfo.footerFields[translations.indicator.unit_of_measurement];
     //this.createTable(chartInfo.selectionsTable, chartInfo.indicatorId, '#selectionsTable', true);
     this.createTable(chartInfo.selectionsTable, tableUnit, chartInfo.indicatorId, '#selectionsTable', true);
     //---#19 addUnitToTableHeaderIfNeeded---stop----------------------------------------------------
